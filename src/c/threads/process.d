8942afbb854f823752234eec7d3b6c9d2d51385f
Marius Gerbershagen <marius.gerbershagen@gmail.com>
2019-10-20 15:28:43 +0200

    add some missing ecl_va_end statements

--- src/c/threads/process.d
+++ src/c/threads/process.d
@@ -434,9 +434,9 @@ cl_object
 mp_process_preset(cl_narg narg, cl_object process, cl_object function, ...)
 {
   ecl_va_list args;
-  ecl_va_start(args, function, narg, 2);
   if (narg < 2)
     FEwrong_num_arguments(@[mp::process-preset]);
+  ecl_va_start(args, function, narg, 2);
   assert_type_process(process);
   process->process.function = function;
   process->process.args = cl_grab_rest_args(args);
@@ -648,7 +648,6 @@ mp_process_run_function(cl_narg narg, cl_object name, cl_object function, ...)
   cl_object process;
   cl_object rest;
   ecl_va_list args;
-  ecl_va_start(args, function, narg, 2);
   if (narg < 2)
     FEwrong_num_arguments(@[mp::process-run-function]);
   if (CONSP(name)) {
@@ -656,6 +655,7 @@ mp_process_run_function(cl_narg narg, cl_object name, cl_object function, ...)
   } else {
     process = mp_make_process(2, @':name', name);
   }
+  ecl_va_start(args, function, narg, 2);
   rest = cl_grab_rest_args(args);
   ecl_va_end(args);
   cl_apply(4, @'mp::process-preset', process, function,
